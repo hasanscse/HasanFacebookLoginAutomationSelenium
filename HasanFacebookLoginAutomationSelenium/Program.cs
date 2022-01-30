e7d2859d38c045b137af1530b08ebb74bb8d0742
Your Name <you@example.com>
2022-01-31 01:40:18 +0600

    Add all my files

---
+++ HasanFacebookLoginAutomationSelenium/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver(@"F:\");
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+            driver.Manage().Window.Maximize();
+
+
+            driver.FindElement(By.Id("email")).SendKeys("hasan.assignment@gmail.com");
+            System.Threading.Thread.Sleep(4000);
+            driver.FindElement(By.Id("pass")).SendKeys("123456" + Keys.Enter);
+            System.Threading.Thread.Sleep(4000);
+
+        }
+    }
+}
+
